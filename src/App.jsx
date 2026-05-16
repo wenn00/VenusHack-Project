73eaf6773a1eafbcdbd8b2bef58d5840c5744055
Meredith <pinkelephantloopsy@gmail.com>
2026-05-16 14:36:52 -0700

    More backend setup, basic frontend

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <header style={{ padding: "1rem", background: "#ffb6b6", color: "white" }}>
+        <h1>Connecting the Dots</h1>
+      </header>
+      <main style={{ padding: "2rem" }}>
+        <p>This is the frontend.</p>
+      </main>
+    </>
+  );
+}
+
+export default App;
